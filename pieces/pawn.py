c8a80a36eefd75a649fa47c1ac93593317e0f42b
Yoken0 <kenzomatsuo@gmail.com>
2024-02-08 12:39:24 -0500

    added pawn and rook move legality, please give feedback on pawn as needed.

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -4,4 +4,20 @@ from piece import Piece
 class Pawn(Piece):
     # TODO
     def can_move(self, new_position):
-        pass
+        dx = abs(new_position[0] - self.position[0])
+        dy = abs(new_position[1] - self.position[1])
+        # check if incorrect dy change and if dy and dx change accordingly
+        if (dy > 3 or (dy >=2 and dx > 1)):
+            return False
+        # check if moved on row 1 or row 7
+        if (self.position[0] == 7 and not self.color and dy == 2):
+            return True
+        if (self.position[0] == 1 and self.color and dy == 2):
+            return True
+        # lastly check if dx change illegaly
+        if(dx >= 1 and dy == 0):
+            return False
+        return True
+        
+
+
--- pieces/rook.py
+++ pieces/rook.py
@@ -4,4 +4,13 @@ from piece import Piece
 class Rook(Piece):
     # TODO
     def can_move(self, new_position):
-        pass
+        # moves horizontally
+        dx = abs(new_position[0] - self.position[1])
+        # move vertically
+        dy = abs(new_position[0] - self.position[1])
+        if((dy > 0 and dx != 0) or dx > 0 and dy != 0):
+            return False
+        else:
+            return True
+
+
