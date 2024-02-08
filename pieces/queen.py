70e95d48ef5462218f994b21dfebc9f92bbec7e9
Yoken0 <kenzomatsuo@gmail.com>
2024-02-08 13:08:23 -0500

    Queen movement protype please revise first

--- pieces/queen.py
+++ pieces/queen.py
@@ -4,4 +4,9 @@ from piece import Piece
 class Queen(Piece):
     # TODO
     def can_move(self, new_position):
-        pass
+        dx = abs(new_position[0] - self.position[0])
+        dy = abs(new_position[1] - self.position[1])
+        # queen check movement prototype
+        if(((dy > 0 and dx != 0) or (dx > 0 and dy != 0)) and dy != dx):
+            return False
+        return True
