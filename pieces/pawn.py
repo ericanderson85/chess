b362ad2786b8d3b926759d5ae16d65653ecc76db
ericanderson85 <ericandersonnn@icloud.com>
2024-02-08 13:33:02 -0500

    Fixed pawn logic

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -2,22 +2,30 @@ from piece import Piece
 
 
 class Pawn(Piece):
-    # TODO
     def can_move(self, new_position):
-        dx = abs(new_position[0] - self.position[0])
-        dy = abs(new_position[1] - self.position[1])
-        # check if incorrect dy change and if dy and dx change accordingly
-        if (dy > 3 or (dy >=2 and dx > 1)):
+
+        row = self.position[0]
+        col = self.position[1]
+
+        dy = abs(new_position[0] - row)  # Absolute change in row
+        dx = new_position[1] - col  # Change in column
+
+        if self.color == True:  # The pawn is white
+            if row == 1:  # Pawn is on row 1
+                if dy != 1 and dy != 2:  # Can only move up 1 or up 2
+                    return False
+            else:
+                if dy != 1:  # dy can only be 1 on any other row
+                    return False
+        else:  # The pawn is black
+            if row == 6:  # Pawn is on row 6
+                if dy != -1 and dy != -2:  # Can only move down 1 or down 2
+                    return False
+            else:
+                if dy != -1:  # dy can only be -1 on any other row
+                    return False
+        # Can only move left or right once, can't move twice and diagonally
+        if dx > 1 or abs(dy) == 2 and dx > 0:
             return False
-        # check if moved on row 1 or row 7
-        if (self.position[0] == 7 and not self.color and dy == 2):
-            return True
-        if (self.position[0] == 1 and self.color and dy == 2):
-            return True
-        # lastly check if dx change illegaly
-        if(dx >= 1 and dy == 0):
-            return False
+
         return True
-        
-
-
