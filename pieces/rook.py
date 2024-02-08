0bcde177a0bd21dde2443904137383fbdbb729c9
Yoken0 <kenzomatsuo@gmail.com>
2024-02-08 12:41:02 -0500

    hotfix on rook.py

--- pieces/rook.py
+++ pieces/rook.py
@@ -5,9 +5,9 @@ class Rook(Piece):
     # TODO
     def can_move(self, new_position):
         # moves horizontally
-        dx = abs(new_position[0] - self.position[1])
+        dx = abs(new_position[0] - self.position[0])
         # move vertically
-        dy = abs(new_position[0] - self.position[1])
+        dy = abs(new_position[1] - self.position[1])
         if((dy > 0 and dx != 0) or dx > 0 and dy != 0):
             return False
         else:
