fd8c30cc2035b1b3e69bafd098d9e9a04b4b0dcc
ericanderson85 <ericandersonnn@icloud.com>
2024-02-08 12:04:24 -0500

    Implement King logic

--- pieces/king.py
+++ pieces/king.py
@@ -2,6 +2,8 @@ from piece import Piece
 
 
 class King(Piece):
-    # TODO
     def can_move(self, new_position):
-        pass
+        # King can move one column and one row at a time
+        dx = abs(new_position[0] - self.position[0])
+        dy = abs(new_position[1] - self.position[1])
+        return dx <= 1 and dy <= 1
